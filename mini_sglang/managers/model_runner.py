fix: update memory usage calculation and improve token handling in requests
Signed-off-by: jason-fxz <jason341132@qq.com>

mini_sglang/managers/model_runner.py
@@ -57,7 +57,8 @@ class ModelRunner:
         free_size, total_size = torch.cuda.mem_get_info(
             self.gpu_id
         )  # free memory in bytes
-        used_size = total_size - free_size
+        # used_size = total_size - free_size
+        used_size = torch.cuda.memory_allocated(self.gpu_id)
         num_kv_heads = self.model_config.num_kv_heads // self.tp_size
 
         cell_size = (
@@ -160,8 +161,7 @@ class ModelRunner:
 
         # Update the batch info with the output ids
         for i, req in enumerate(batch.reqs):
-            req.token_ids.append(output_ids[i])
-            req.last_token_id = output_ids[i].item()
+            req.token_ids.append(output_ids[i].item())
             req.prefix_indices = self.req_to_token_pool.req_to_token[req.req_pool_idx][
                 : len(req) - 1
             ]
@@ -182,8 +182,7 @@ class ModelRunner:
 
         # Update the batch info with the output ids
         for i, req in enumerate(batch.reqs):
-            req.token_ids.append(output_ids[i])
-            req.last_token_id = output_ids[i].item()
+            req.token_ids.append(output_ids[i].item())
             req.prefix_indices = self.req_to_token_pool.req_to_token[req.req_pool_idx][
                 : len(req) - 1
             ]

mini_sglang/managers/req_info.py
@@ -27,7 +27,6 @@ class Req:
         self.req_id = next(Req.counter)
         self.status = ReqStatus.WAITING
         self.token_ids = copy(token_ids)  # upd
-        self.last_token_id = token_ids[-1] if token_ids else None  # upd
         self.max_tokens = sampling_params.max_tokens
         self.num_prompt_tokens = len(token_ids)
         self.ignore_eos = sampling_params.ignore_eos
@@ -37,6 +36,10 @@ class Req:
         # prefix info
         # The indices to kv cache for shared prefix
         self.prefix_indices: torch.tensor = torch.tensor([], dtype=torch.int32)  # upd
+
+    @property
+    def last_token_id(self) -> int:
+        return self.token_ids[-1] if self.token_ids else None
 
     @property
     def num_tokens(self) -> int: